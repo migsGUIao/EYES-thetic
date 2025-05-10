688a2ceec29d8d0b3becb1e8e05879fa544ed79c
katrombs <kathleen_romblon@dlsu.edu.ph>
2025-05-11 01:20:51 +0800

    base mobile UI
    > includes major pages
    > placeholders for grid views and dynamic text

---
+++ mobile/app/src/main/java/com/example/eyesthetic/LandingActivity.java
@@ -0,0 +1,34 @@
+package com.example.eyesthetic;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LandingActivity extends AppCompatActivity {
+    Button landingBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_landing);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.landing), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        landingBtn = findViewById(R.id.landingBtn);
+    }
+
+    public void toLogin(View v) {
+        //Function to go to login
+    }
+
+}
